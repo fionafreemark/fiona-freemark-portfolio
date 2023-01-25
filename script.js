624e681095b6716e13bea4acf91cc9ea8c32f2ce
fiona freemark <fiona.freemark@gmail.com>
2023-01-24 22:01:25 -0500

    addressing mobile menu issues

--- script.js
+++ script.js
@@ -5,4 +5,25 @@ const menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', function () {
   menuButton.classList.toggle("active");
 
+  const navButton = document.querySelector('.mobile-nav-menu');
+  navButton.addEventListener('click', function () {
+    navButton.classList.toggle("inactive");
+    console.log('off');
+  })
 });
+
+// Mobile Menu Scroll Colour Change
+
+const scrollElements = document.querySelectorAll('scroll');
+
+const watch = document.querySelector('.watch');
+
+function obCallBack(payload) {
+  console.log(payload[0].isIntersecting);
+  if (payload[0].isIntersecting === true) {
+    scrollElements.classList.add('.scrolled');
+  }
+}
+
+const ob = new IntersectionObserver(obCallBack);
+ob.observe(watch);
