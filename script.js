f90651d3fbeb9095bc1113ebac2faabd209b7bbe
fiona freemark <fiona.freemark@gmail.com>
2023-01-24 21:00:42 -0500

    mobile menu & associate js added, fixed position menu button in corner

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile Menu Toggle
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', function () {
+  menuButton.classList.toggle("active");
+
+});
